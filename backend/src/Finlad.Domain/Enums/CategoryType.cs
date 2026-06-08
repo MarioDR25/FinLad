225e18ceda5281cdbaf9bd18ebc855be49d8f896
Mario Daza <mariodaza525@gmail.com>
2026-06-08 19:19:02 +0200

    feat: add CategoryType enum with default categories

---
+++ backend/src/Finlad.Domain/Enums/CategoryType.cs
@@ -0,0 +1,15 @@
+namespace Finlad.Domain.Enums;
+
+public enum CategoryType
+{
+    Food,
+    Transport,
+    Utilities,
+    Housing,
+    Entertainment,
+    Health,
+    Education,
+    Shopping,
+    Salary,
+    Other
+}
