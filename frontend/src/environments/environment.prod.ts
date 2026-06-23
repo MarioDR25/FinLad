a610fe5ca7a39390d806efd1817ecaa564cc3ab8
Mario Daza <mariodaza525@gmail.com>
2026-06-23 21:43:14 +0200

    chore: update production environment URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'https://tu-api-en-produccion.com/api',
+    apiUrl: 'https://finlad-api.fly.dev/api',
     googleClientId: '436037394685-o8kdsjcbhr6sjdqfpl439iuo8tmf8ihe.apps.googleusercontent.com'
 }
